1493559aa7eb9993896dcef1dd461eaf65457d91
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-23 22:06:49 +0300

    WIP: user page

--- app/src/pages/lectorPage.tsx
+++ app/src/pages/lectorPage.tsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useAppSelector } from "../hooks/redux";
 
 export default function LectorPage() {
     const lectorId = useParams()
+
+    const LECTOR = useAppSelector((state) => state.lector)
 
     return (<>
         <div className="lector">
@@ -11,7 +14,7 @@ export default function LectorPage() {
             animate={{opacity: 1}}
             transition={{duration: 1}}
             className="lector--title"
-            >Фамилия Имя Отчество {lectorId.id}</motion.h1>
+            >{LECTOR.lectorName} {lectorId.id}</motion.h1>
             <div className="metrics">
                 <motion.div 
                 initial={{opacity: 0, y: 10}}
@@ -19,7 +22,7 @@ export default function LectorPage() {
                 transition={{duration: 1}}
                 className="metrics--spec">
                     <p className="metrics--spec__name">Специальность</p>
-                    <p className="metrics--spec__value">Психолог</p>
+                    <p className="metrics--spec__value">{LECTOR.lectorCategory}</p>
                 </motion.div>
                 <motion.div 
                 initial={{opacity: 0, y: 10}}
@@ -28,7 +31,7 @@ export default function LectorPage() {
                 className="metrics--rating">
                     <p className="metrics--rating__name">Рейтинг</p>
                     <p className="metrics--rating__value" 
-                    style={{backgroundColor: "#F9E07C"}}>9.8/10</p>
+                    style={{backgroundColor: "#F9E07C"}}>{LECTOR.rating}</p>
                 </motion.div>
             </div>
             <button className="lector--record" style={{backgroundColor: "#FFD4A8"}}>записаться</button>
--- app/src/pages/userPage.tsx
+++ app/src/pages/userPage.tsx
@@ -1,10 +1,37 @@
 import { useParams } from "react-router-dom"
+import { motion } from "framer-motion"
+import { evaluatePerformance } from "../devtools/colorUtils"
 
 export default function UserPage() {
     const userId = useParams()
+
     return (<>
         <div className="user">
-            {userId.id}
+            <motion.h1
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 1}}
+            className="user--title"
+            >{userId.id}</motion.h1>
+            <motion.div 
+            initial={{opacity: 0, y: 10}}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 1, delay: 0.5}}
+            className='metrics'>
+                <div className="metrics--happiness">
+                    <p className='metrics--happiness__name'>Рейтинг довольства</p>
+                    <p className='metrics--happiness__value' style={{backgroundColor: evaluatePerformance(87)}}>87/100</p>
+                </div>
+                <div className="metrics--physical">
+                    <p className='metrics--physical__name'>Физическое здоровье</p>
+                    <p className='metrics--physical__value' style={{backgroundColor: evaluatePerformance(73)}}>73/100</p>
+                </div>
+                <div className="metrics--mental">
+                    <p className='metrics--mental__name'>Психическое здоровье</p>
+                    <p className='metrics--mental__value' style={{backgroundColor: evaluatePerformance(53)}}>53/100</p>
+                </div>
+            </motion.div>
+            {/* <button className="lector--record" style={{backgroundColor: "#FFD4A8"}}>записаться</button> */}
         </div>
     </>)
 }
