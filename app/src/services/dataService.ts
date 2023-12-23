cfa89a0ddc84e67f95bb49af912ee55c04801ca8
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-24 00:01:29 +0300

    FIX: get lecture

--- app/src/services/dataService.ts
+++ app/src/services/dataService.ts
@@ -11,7 +11,7 @@ export const DataService = createApi({
 		getLecturesByDate : build.query<any, string>({
 			query : (data) => ({
 				// 2023-12-23
-				url : `/meet/lecture/all?date=${data}`,
+				url : `/meet/lecture/all?date=${data.slice(0, 19)}`,
 				headers : {
 					"Content-Type": "application/json",
 				},
