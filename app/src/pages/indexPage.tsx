b078207af41afb0d480046e926a13db59c4bc1ad
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-23 11:05:53 +0300

    FIX: user model separation

--- app/src/pages/indexPage.tsx
+++ app/src/pages/indexPage.tsx
@@ -1,8 +1,21 @@
 import { useNavigate } from "react-router-dom"
 import { motion } from "framer-motion"
+import { useAppSelector } from "../hooks/redux"
+import { EUserRole } from "../models/EUserRole"
 
 export default function IndexPage() {
     const navigator = useNavigate()
+
+    const USER = useAppSelector((state) => state.user)
+
+    const getText = (role: EUserRole) => {
+        switch (role) {
+            case EUserRole.moderator: return "_пользователи"
+            case EUserRole.speaker: return "_мои клиенты"
+            case EUserRole.worker || EUserRole.none: return "_специалисты"
+            default: return "_специалисты"
+        }
+    }
 
     return (<>
         <div className='index'>
@@ -19,15 +32,18 @@ export default function IndexPage() {
                 transition={{duration: 1, delay: 0.1}}
                 onClick={() => {navigator('lectures')}}
                 className="index--card index--card__lectures">
-                    <p>_лекции</p>
+                    <p>{USER.role === EUserRole.speaker
+                    ? "_мои лекции"
+                    : "_лекции"
+                    }</p>
                 </motion.div>
                 <motion.div 
                 initial={{opacity: 0, y: 20}}
                 animate={{opacity: 1, y: 0}}
                 transition={{duration: 1, delay: 0.2}}
-                onClick={() => {navigator('lectors')}}
+                onClick={() => {USER.role === EUserRole.moderator ? navigator('users') : navigator('lectors')}}
                 className="index--card index--card__lectors">
-                    <p>_специалисты</p>
+                    <p>{getText(USER.role)}</p>
                 </motion.div>
                 <motion.div 
                 initial={{opacity: 0, y: 30}}
--- app/src/store/reducers/IUserSlice.ts
+++ app/src/store/reducers/IUserSlice.ts
@@ -6,7 +6,7 @@ const initialState : IUser = {
 	id: 0,
 	fio: "nothing",
 	login : "nothing",
-	role : EUserRole.moderator
+	role : EUserRole.speaker
 }
 
 export const IUserSlice = createSlice({
