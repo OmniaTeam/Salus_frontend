23dce8d08f9011a9d72c7fdaab27859e472d1597
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-12-23 17:30:43 +0300

    WIP: data service

--- app/src/services/dataService.ts
+++ app/src/services/dataService.ts
@@ -1,11 +1,45 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { ILecture } from "../models/ILecture";
+import { IMeetup } from "../models/IMeetup";
 
 export const DataService = createApi({
-	reducerPath : "auth-service",
+	reducerPath : "data-service",
 	baseQuery : fetchBaseQuery({
-		baseUrl : "https://involutio.the-omnia.ru/api/v3"
+		baseUrl : "https://salus.the-omnia.ru/api/v3"
 	}),
-	endpoints : (/*build*/) => ({})
+	endpoints : (build) => ({
+		getLecturesByDate : build.query<ILecture[], any>({
+			query : () => ({
+				url : "/",
+				headers : {
+					"Content-Type": "application/json",
+				},
+				method: "GET"
+			})
+		}),
+		getMeetupsByDate : build.query<IMeetup[], any>({
+			query : () => ({
+				url : "/",
+				headers : {
+					"Content-Type": "application/json",
+				},
+				method: "GET"
+			})
+		}),
+		getWorkerMeetups : build.query<IMeetup[], number>({
+			query : ( workerId ) => ({
+				url : `/${workerId}`,
+				headers : {
+					"Content-Type": "application/json",
+				},
+				method: "GET"
+			})
+		})
+	})
 })
 
-export const {} = DataService;
+export const {
+	useGetLecturesByDateQuery,
+	useGetMeetupsByDateQuery,
+	useGetWorkerMeetupsQuery
+} = DataService;
